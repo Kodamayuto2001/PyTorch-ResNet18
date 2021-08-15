2d2b63eaf5f2b32a00eb660cafe95dbb324aa2cd
kodamayuto <yuto2001gyokugan@gmail.com>
2021-08-15 11:26:17 +0900

    データセットを移動

--- ResNet18.py
+++ ResNet18.py
@@ -1,6 +1,14 @@
 import torch 
+import cv2
 
 class Net(torch.nn.Module):
-    def __init__(self):
+    def __init__(self,channel_in,channel_out):
         super(Net,self).__init__()
-        
+        channel = channel_out
+        self.conv1 = torch.nn.Conv2d(channel_in,channel,kernel_size=(1,1))
+
+    def forward(self,x):
+        print(x.size())
+        return x
+
+
